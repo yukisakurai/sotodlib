af7bf5f5f6df0c49e75d46bb4427b1eda57ad935
Theodore Kisner <tskisner.public@gmail.com>
2021-09-16 12:57:41 -0700

    Work on new data interfaces.

---
+++ sotodlib/toast/spt3g/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2020-2021 Simons Observatory.
+# Full license can be found in the top level "LICENSE" file.
+"""Simons Observatory TOAST / SPT3G helper classes.
+
+"""
+
+from .load import ImportSimMeta, ImportSimData
+from .save import ExportSimMeta, ExportSimData
